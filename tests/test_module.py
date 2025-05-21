a3602a73f71ab82934eaab0827c0b04f1a6a9076
Raimon Esteve <raimonesteve@gmail.com>
2025-05-21 10:42:33 +0200

    lower slugify

--- tests/test_module.py
+++ tests/test_module.py
@@ -3,12 +3,24 @@
 # this repository contains the full copyright notices and license terms.
 
 from trytond.modules.company.tests import CompanyTestMixin
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.pool import Pool
 
 
 class ProductEsaleCategoriesTestCase(CompanyTestMixin, ModuleTestCase):
     'Test ProductEsaleCategories module'
     module = 'product_esale_categories'
 
+    @with_transaction()
+    def test_slugify(self):
+        'Test slugify'
+        pool = Pool()
+        Category = pool.get('product.category')
+
+        category = Category()
+        category.name = 'Menu Demo'
+        category.slug = None
+        category.on_change_name()
+        self.assertEqual(category.slug, 'menu-demo')
 
 del ModuleTestCase
